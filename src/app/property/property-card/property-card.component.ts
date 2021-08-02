f7b7b79ba7e910fa6b91662066572d1c86596b63
David <david.morkos@global.ntt>
2021-08-02 16:36:24 +1000

    initalise

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-property-card',
+  //template: '<h1> I am a card </h1>',
+  templateUrl: 'property-card.component.html',
+  //styles: ['h1 {font-weight: normal;}']
+  styleUrls: ['property-card.component.css']
+})
+export class PropertyCardComponent {
+
+  Proprty: any = {
+    "Id": 1,
+    "Type": "House",
+    "Name" : "David",
+    "Price": 12000
+  }
+
+}
