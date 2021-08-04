ce494656a7dedb086727e6b075383614f453063f
David <david.morkos@global.ntt>
2021-08-04 13:49:07 +1000

    added registerration form and added user to local storage

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -11,6 +11,7 @@ export class AddPropertyComponent implements OnInit {
   constructor(private router: Router ) { }
 
   ngOnInit() {
+      
   }
 
   onBack() {
---
+++ src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-register',
+  templateUrl: './user-register.component.html',
+  styleUrls: ['./user-register.component.css']
+})
+export class UserRegisterComponent implements OnInit {
+
+  registrationForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  user: any = {};
+
+  ngOnInit() {
+    // this.registrationForm = new FormGroup(
+    //   {
+    //     userName: new FormControl(null, Validators.required),
+    //     email: new FormControl(null, [Validators.required, Validators.email]),
+    //     password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    //     confirmPassword: new FormControl(null, [Validators.required]),
+    //     mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+    //   }, this.passwordMatchingValidator
+    //);
+    this.createRegistrationForm()
+  }
+  createRegistrationForm() {
+    this.registrationForm = this.fb.group({
+      userName: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      confirmPassword: [null, Validators.required],
+      mobile: [null, [Validators.required, Validators.maxLength(10)]]
+    }, {Validators: this.passwordMatchingValidator})
+  }
+
+  passwordMatchingValidator(fc: AbstractControl): ValidationErrors | null {
+    return fc.get('password')?.value === fc.get('confirmPassword')?.value ? null :
+      { notmatched: true }
+  };
+
+  get userName() {
+    return this.registrationForm.get('userName') as FormControl;
+  }
+  get email() {
+    return this.registrationForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.registrationForm.get('password') as FormControl;
+  }
+
+  get confirmPassword() {
+    return this.registrationForm.get('confirmPassword') as FormControl;
+  }
+
+  get mobile() {
+    return this.registrationForm.get('mobile') as FormControl;
+  }
+
+  onSubmit() {
+     console.log(this.registrationForm);
+     this.user = Object.assign(this.user, this.registrationForm.value);
+     this.addUser(this.user);
+     this.registrationForm.reset();
+  }
+
+  addUser(user: any) {
+    let users = [];
+    if(localStorage.getItem('Users')) {
+      users = JSON.parse(localStorage.getItem('Users') as string);
+      users = [user, ...users];
+    } else {
+      users = [user];
+    }
+    localStorage.setItem('Users', JSON.stringify(users));
+  }
+
+}
