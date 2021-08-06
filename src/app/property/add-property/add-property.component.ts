5ca73de2a6d94a86b68968beab888ecf1434487d
David <david.morkos@global.ntt>
2021-08-06 10:42:33 +1000

    added new prop

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {Router} from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
+import { IProperty } from '../IProprty.interface';
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
@@ -10,6 +11,18 @@ import { TabsetComponent } from 'ngx-bootstrap/tabs';
 export class AddPropertyComponent implements OnInit {
   @ViewChild('Form') addPropertyForm: NgForm | undefined
   @ViewChild('formTabs') formTabs!: TabsetComponent;
+
+  propertyType: Array<string> = ['House', 'Apartment', 'Duplex'];
+  FurnishType: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
+  readyToMoveType: Array<string> = ['East', 'West', 'South', "North"];
+  propertyView: IProperty = {
+    Id: 0,
+    Name: '',
+    Price: 0,
+    SellRent: 0,
+    Type: ""
+  };
+
   constructor(private router: Router ) { }
 
   ngOnInit() {
