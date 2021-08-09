24cc6a1f5f5482de880e549fac9d7437622ac53a
David <david.morkos@global.ntt>
2021-08-09 14:23:30 +1000

    added models

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -20,16 +20,16 @@ export class AddPropertyComponent implements OnInit {
   nextClicked!: Boolean;
 
   propertyView: IPropertyBase = {
-    Id: 0,
-    Name: '',
-    Price: 0,
-    SellRent: 0,
-    PType: "",
-    FType: "",
-    BHK: 0,
-    BuiltArea: 0,
-    City: "",
-    RTM:0
+    Id: null,
+    Name: null,
+    Price: null,
+    SellRent: null,
+    PType: null,
+    FType: null,
+    BHK: null,
+    BuiltArea: null,
+    City: null,
+    RTM: null
   };
 
   constructor(private fb: FormBuilder, private router: Router ) { }
@@ -41,40 +41,142 @@ export class AddPropertyComponent implements OnInit {
   CreateAddPropertyForm() {
     this.addPropertyForm = this.fb.group({
       BasicInfo: this.fb.group({
-        SellRent: [null, Validators.required],
+        SellRent: ['1', Validators.required],
+        BHK: [null, Validators.required],
         PType: [null, Validators.required],
-        Name: [null, Validators.required]
+        FType: [null, Validators.required],
+        Name: [null, Validators.required],
+        City: [null, Validators.required]
       }),
       PriceInfo: this.fb.group({
         Price: [null, Validators.required],
-        BuiltArea: [null, Validators.required]
+        BuiltArea: [null, Validators.required],
+        CarpetArea: [null],
+        Security: [null],
+        Maintenance: [null],
+      }),
+      AddressInfo: this.fb.group({
+        FloorNo: [null],
+        TotalFloor: [null],
+        Address: [null, Validators.required],
+        LandMark: [null]
+      }),
+      OtherInfo: this.fb.group({
+        RTM: [null, Validators.required],
+        PosessionOn: [null],
+        AOP: [null],
+        Gated: [null],
+        MainEntrance: [null],
+        Description: [null]
       })
-
     })
   }
   // ------------------
   // Getter Methods
+  // # Region Getter methods
+      // Region <FormGroups>
   // ------------------
-  get BasicInfo() {
-    return this.addPropertyForm.controls.BasicInfo as FormGroup;
+//#region <Getter Methods>
+  //#region <FormGroups>
+    get BasicInfo() {
+      return this.addPropertyForm.controls.BasicInfo as FormGroup;
+    }
+
+    get PriceInfo() {
+      return this.addPropertyForm.controls.PriceInfo as FormGroup;
+    }
+
+    get AddressInfo() {
+      return this.addPropertyForm.controls.AddressInfo as FormGroup;
+    }
+
+    get OtherInfo() {
+      return this.addPropertyForm.controls.OtherInfo as FormGroup;
+    }
+
+
+  //#endregion
+  //#region <FormControl>
+  get SellRent() {
+    // console.log(this.addPropertyForm.controls.BasicInfo?.controls.SellRent.status)
+    return this.BasicInfo.controls.SellRent as FormControl;
   }
 
-  get SellRent() {
-    console.log(this.addPropertyForm.controls.SellRent)
-    return this.addPropertyForm.controls.SellRent as FormGroup;
+  get Price() {
+    return this.PriceInfo.controls.Price as FormControl;
   }
+
+  get RTM() {
+    return this.OtherInfo.controls.RTM as FormControl;
+  }
+
+  get BHK() {
+    return this.BasicInfo.controls.BHK as FormControl;
+  }
+
+  get Name() {
+    return this.BasicInfo.controls.Name as FormControl;
+  }
+
+  get PType() {
+    return this.BasicInfo.controls.PType as FormControl;
+  }
+
+  get FType() {
+    return this.BasicInfo.controls.FType as FormControl;
+  }
+
+  get City() {
+    return this.BasicInfo.controls.City as FormControl;
+  }
+
+  get Address() {
+    return this.AddressInfo.controls.Address as FormControl;
+  }
+
+  //#endregion
 
   onBack() {
     this.router.navigate(['/']);
   }
   onSubmit() {
-    console.log("Congrats form submitted");
-    console.log('SellRent=' + this.addPropertyForm?.value.BasicInfo.SellRent);
-    console.log(this.addPropertyForm);
+    this.nextClicked=true;
+    if(this.allTabsValid()) {
+      console.log("Congrats form submitted");
+      console.log('SellRent=' + this.addPropertyForm?.value.BasicInfo.SellRent);
+      console.log(this.addPropertyForm);
+    } else {
+      console.log("Please review the form and provide all valid entries");
+    }
+
   }
 
+  allTabsValid(): boolean {
+
+    if(this.BasicInfo.invalid) {
+      this.formTabs.tabs[0].active = true;
+      return false;
+    }
+
+    if(this.PriceInfo.invalid) {
+      this.formTabs.tabs[1].active = true;
+      return false;
+    }
+
+    if(this.AddressInfo.invalid) {
+      this.formTabs.tabs[2].active = true;
+      return false;
+    }
+
+    if(this.OtherInfo.invalid) {
+      this.formTabs.tabs[3].active = true;
+      return false;
+    }
+    return true;
+  }
   selectTab(tabId: number, IsCurrentTabValid: boolean) {
     this.nextClicked = true;
+    console.log(this.BasicInfo);
     if(IsCurrentTabValid) {
       this.formTabs.tabs[tabId].active = true;
     }
